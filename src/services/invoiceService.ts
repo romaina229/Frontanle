475b24e475cd0ac0ed1693f381de262db76b30b0
romaina229 <nounonvenanromain@gmail.com>
2026-01-28 03:40:34 +0100

    voir erreur

--- src/services/invoiceService.ts
+++ src/services/invoiceService.ts
@@ -2,9 +2,9 @@ import api from './api';
 
 const invoiceService = {
   getAll: (params?: any) => api.get('/invoices', { params }),
-  getById: (id: number) => api.get(`/invoices/\${id}\`),
-  download: (id: number) => api.get(\`/invoices/\${id}/download\`, { responseType: 'blob' }),
-  generate: (saleId: number) => api.post(\`/sales/\${saleId}/invoice\`),
+  getById: (id: number) => api.get(`/invoices/${id}`),
+  download: (id: number) => api.get(`/invoices/${id}/download`, { responseType: 'blob' }),
+  generate: (saleId: number) => api.post(`/sales/${saleId}/invoice`),
 };
 
 export default invoiceService;
