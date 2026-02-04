60afae2808423feb2d3314650d0447d8bf8abce2
romaina229 <nounonvenanromain@gmail.com>
2026-02-05 00:32:36 +0100

    logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -17,7 +17,7 @@ import { toast } from 'react-hot-toast';
 import { logout } from '../../store/slices/authSlice';
 import { RootState } from '../../store';
 import api from '../../services/api';
-import logo = '/src/assets/images/logo.png';
+import logo from '../../assets/images/logo.png';
 
 
 const { Header: AntHeader } = Layout;
